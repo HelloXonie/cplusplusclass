6daea307f92f134a1ea8729e8f1dd281447fd18a
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-17 22:40:34 -0700

    completed code

--- ch2_ex1/main.cpp
+++ ch2_ex1/main.cpp
@@ -1,5 +1,5 @@
 /*
-Name: Xonie Martin
+Name: Xonie
 Description: Purpose of this program is to calculate 
 the gross pay for a College and then display it.
 Hours and rate will be fixed values.
@@ -10,10 +10,16 @@ Hours and rate will be fixed values.
 using namespace std;
 
 int main() {
+	string companyName = "De Anza College";
+	int hours = 40;
+	int rate = 100;
+	int grossPay = hours * rate; // Calculation for Gross Pay
+	
+	//Display results
+	cout << "Company Name: " << companyName << endl;
+	cout << "Hours worked: " << hours << endl;
+	cout << "Rate per hour: " << rate << endl;
+	cout << "Your " << companyName << " gross pay is: " << grossPay << " dollars." << endl;
 
-
-
-
-
-
+	return 0;
 }
