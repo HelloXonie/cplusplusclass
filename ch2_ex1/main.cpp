754d804b62ac2d2749bc96affbea06c08f27a87d
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-17 22:19:15 -0700

    ex 1

---
+++ ch2_ex1/main.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Xonie Martin
+Description: Purpose of this program is to calculate 
+the gross pay for a College and then display it.
+Hours and rate will be fixed values.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+}
