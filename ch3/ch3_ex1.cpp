ead29c1f60968a55c8b3bbdf32315f8873bfa223
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-21 18:17:14 -0700

    final submission for chapter 3 ex 1

--- ch3/ch3_ex1.cpp
+++ ch3/ch3_ex1.cpp
@@ -1,5 +1,7 @@
 /* Name: Xonie
-Description:
+Description: The purpose of this program is to take user inputs,
+store those values into variables then give an output
+based on calculations made with those inputs
 
 */
 
@@ -7,10 +9,45 @@ Description:
 using namespace std;
 
 int main() {
+	
+	int hours;
+	double rate;
 
+	// Prompt the user to enter in hours, and take an input
+	cout << "Hello, how many hours have you worked?" << endl;
+	cin >> hours; // User input value for hours.
+	
+	// Prompt the user to enter in rate, and take an input.
+	cout << "Thank you, and what is your rate per hour?" << endl;
+	cin >> rate; // User input value for rate.
+	
+	// Calculation for gross pay.
+	double grossPay = hours * rate;
 
+	// Display the output for hours, rate, and gross pay.
+	cout << "Hours: " << hours << endl;
+	cout << "Rate: " << rate << endl;
+	cout << "Your gross pay is: " << grossPay << endl;
 
-
-
-
+	return 0;
 }
+
+/*
+Hello, how many hours have you worked?
+12
+Thank you, and what is your rate per hour?
+42.58
+Hours: 12
+Rate: 42.58
+Your gross pay is: 510.96
+
+--
+
+Hello, how many hours have you worked?
+40
+Thank you, and what is your rate per hour?
+48
+Hours: 40
+Rate: 48
+Your gross pay is: 1920
+*/
