9f43f45ca59619013dc7224cbadd89244c6103d5
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-22 21:02:46 -0700

    progam challenge 2 final submission

--- programchallengech2/challenge2_salesprediction.cpp
+++ programchallengech2/challenge2_salesprediction.cpp
@@ -8,13 +8,14 @@ Problem Number: 2 - Sales Prediction
 using namespace std;
 
 int main() {
-	//
 	
-	double totalSales = 8600000;
+	// Calculate the east coast sales prediction.
+	double totalSales = 8.6; // 8.6 instead of 8600000 to avoid scientific notation.
 	double eastCoastSales = .58 * totalSales;
+	
+	// Display the output
+	cout << "The company is on track to make: $" << totalSales << " million this year." << endl;
+	cout << "Based on past performance the East Coast division is projected to make: $" << eastCoastSales << " million." << endl;
 
-
-
-
-
+	return 0;
 }
