9ca1cd56b9164d90f6ff2a4a826e1ecfba9a95bb
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-22 22:26:03 -0700

    challenge number 9

---
+++ programchallengech2/challenge9_cyborgdatatypesizes.cpp
@@ -0,0 +1,20 @@
+/*
+Name: Xonie
+Team Name: PlusPlusUltra
+Assignment Title: Programming challenges Chapter 2
+Problem Number: 9 - Cyborg data type size
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// Displays the size of each data type
+	cout << "The size of a char data type is: " << sizeof(char) << " byte." <<endl;
+	cout << "The size of an int data type is: " << sizeof(int) << " bytes." <<endl;
+	cout << "The size of a double data type is: " << sizeof(float) << " bytes." <<endl;
+	cout << "The size of a float data type is: " << sizeof(double) << " bytes." <<endl;
+
+	return 0;
+}
