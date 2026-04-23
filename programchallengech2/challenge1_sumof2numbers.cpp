0d7546217b731aca1d58e6acdfd2bfa47688e664
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-22 18:21:19 -0700

    challenge 1 sum of 2 numbers

---
+++ programchallengech2/challenge1_sumof2numbers.cpp
@@ -0,0 +1,23 @@
+/*
+Name: Xonie
+Description: Sum of 2 numbers
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// Store values and declare variables
+	int firstNumber = 50;
+	int secondNumber = 100;
+	
+	// Calculation of the total of the numbers added together
+	int total = firstNumber + secondNumber;
+
+	// Display the output
+	cout << "The total of these 2 numbers is: " << total << endl;
+
+	return 0;
+}
