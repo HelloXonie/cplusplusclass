299a3d0cba5264f7204f83c89ced826f8165b0c7
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-22 18:55:36 -0700

    adding changes to assignment 2

--- programchallengech2/challenge2_salesprediction.cpp
+++ programchallengech2/challenge2_salesprediction.cpp
@@ -1,4 +1,16 @@
 /*
 Name: Xonie
-
+Assignment Title: Programming challenges Chapter 2
+Problem Number: 2 - Sales Prediction
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+
+
+}
