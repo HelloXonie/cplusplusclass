db32c075906707478e6d23ace8c1e4293ed45a39
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-22 22:07:24 -0700

    starting challenge 4

---
+++ programchallengech2/challenge4_restaurantbill.cpp
@@ -0,0 +1,27 @@
+/*
+Name: Xonie
+Team Name: PlusPlusUltra
+Assignment Title: Programming challenges Chapter 2
+Problem Number: 4 - Restaurant Bill
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// Calculate each value
+	double mealCost = 88.67;
+	double salesTax = .0675 * mealCost;
+	double subTotal = salesTax + mealCost; // calculate the total meal and tax before calculating the tip
+	double tipCost = .2 * subTotal;
+	double finalCost = tipCost + subTotal;
+
+	// Display the output
+	cout << "Your meal cost: $" << mealCost << endl;
+	cout << "Sales  tax: $" << salesTax << endl;
+	cout << "Subtotal: $" << subTotal << endl;
+	cout << "Final cost: $" << finalCost << endl;
+
+	return 0;
+}
