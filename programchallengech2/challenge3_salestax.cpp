7da76cace0f013a89a3317069bc318d8477c8815
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-22 21:45:03 -0700

    challenge 3 completed

--- programchallengech2/challenge3_salestax.cpp
+++ programchallengech2/challenge3_salestax.cpp
@@ -1,5 +1,6 @@
 /*
 Name: Xonie
+Team Name: PlusPlusUltra
 Assignment Title: Programming challenges Chapter 2
 Problem Number: 3 - Sales Tax
 */
@@ -9,8 +10,17 @@ using namespace std;
 
 int main() {
 
+	// Calculate the sales tax of an item
+	double subtotalPrice = 95.00;
+	double stateSalesTax = 0.04 * subtotalPrice;
+	double countySalesTax = 0.02 * subtotalPrice;
+	double grandtotalPrice = subtotalPrice + stateSalesTax + countySalesTax;
 
+	// Display the output
+	cout << "Your subtotal is: $" << subtotalPrice << endl;
+	cout << "State sales tax is: $" << stateSalesTax << endl;
+	cout << "County sales tax is: $" << countySalesTax << endl;
+	cout << "Your grand total price is: $" << grandtotalPrice << endl;
 
-
-
+	return 0;
 }
