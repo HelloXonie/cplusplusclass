6a4d2ce877f81307c8f2ef1bcfaa8e3a32861e39
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-23 20:41:18 -0700

    challenge 5 submission

---
+++ programchallengech2/challenge5_averageofvalues.cpp
@@ -0,0 +1,30 @@
+/*
+Name: Xonie
+Team Name: PlusPlusUltra - Xonie Martin, Kyungjae Lee
+Assigment title / number: Programming Challenges - Chapter 2
+Challenge 5 - Average of Values
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// Data
+	double a = 28;
+	double b = 32;
+	double c = 37;
+	double d = 24;
+	double e = 33;
+
+	// Sum of 5 values
+	double sum = a + b + c + d + e;
+
+	// Average of 5 values
+	double average = sum / 5;
+
+	// Display output
+	cout << "The average is " << average << endl;
+
+	return 0;
+}
