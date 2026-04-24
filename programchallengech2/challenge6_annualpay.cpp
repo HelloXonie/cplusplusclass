cea1f63e7184b6533f08537ce3b7e297f2c9d329
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-23 20:49:36 -0700

    challenge 6 submission

---
+++ programchallengech2/challenge6_annualpay.cpp
@@ -0,0 +1,21 @@
+/*
+Name: Xonie Martin
+Team Name: PlusPlusUltra - Xonie Martin, Kyungjae Lee
+Assignment Title: Programming challenges Chapter 2
+Problem Number: 6 - Annual Pay
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double payAmount = 2200.0;
+	double payPeriods = 26;
+	double annualPay = payAmount * payPeriods; // Calculation of annual pay
+	
+	// Display Output
+	cout << "The employee's annual pay is " << annualPay << " dollars." << endl;
+
+	return 0;
+}
