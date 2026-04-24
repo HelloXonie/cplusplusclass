a75cea441c96a1e6f5cc6e8e853963f29c8b6564
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-23 21:05:50 -0700

    challenge 7 submission

---
+++ programchallengech2/challenge7_oceanlevels.cpp
@@ -0,0 +1,21 @@
+/*
+Name: Xonie Martin
+Team Name: PlusPlusUltra - Xonie Martin, Kyungjae Lee
+Assignment Title: Programming challenges Chapter 2
+Problem Number: 7 - Ocean Levels
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double Rising_Rate = 1.5; // Rate of ocean level rising per year.
+	
+	// Display output
+	cout << "After 5 years, the ocean level will be " << Rising_Rate * 5 << " millimeters higher." << endl;
+	cout << "After 7 years, the ocean level will be " << Rising_Rate * 7 << " millimeters higher." << endl;
+	cout << "After 10 years. the ocean level will be " << Rising_Rate * 10 << " millimeters higher." << endl;
+
+	return 0;
+}
