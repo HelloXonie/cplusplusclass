976234fa5f79e4d15fe426aab398a3e0fc4c2b3e
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-24 20:57:20 -0700

    challenge 11

---
+++ programchallengech2/challenge11_distancepertankofgas.cpp
@@ -0,0 +1,16 @@
+/*
+Name: Xonie
+Team Name: PlusPlusUltra - Xonie Martin, Kyungjae Lee
+Assignment Title: Programming challenges Chapter 2
+Problem Number: 11 - distance per tank of gas
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//
+
+
+}
