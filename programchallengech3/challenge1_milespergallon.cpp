a3dcfad97381546365866dbc3c99394e2885a517
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-26 07:49:06 -0700

    program challenges chapter 3 initial folder creation

---
+++ programchallengech3/challenge1_milespergallon.cpp
@@ -0,0 +1,22 @@
+/*
+Name: Xonie Martin
+Team Name: PlusPlusUltra - Xonie Martin, Kyungjae Lee
+Assignment Title: Programming challenges Chapter 3
+Problem Number: 1 - Miles Per Gallon
+Description: Purpose of this program is to take an input 
+from the user for the number of gallons of gas a car can hold.
+Then calculate miles per gallon.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+
+}
