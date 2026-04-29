822316f2116a42fc2e37748d311cffdfb1f17850
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-28 21:18:44 -0700

    starting ex 3

---
+++ ch3/ch3_ex3.cpp
@@ -0,0 +1,30 @@
+/*
+Name: Xonie
+Team Name: Plus Plus Ultra - Xonie Martin, Kyungjae Lee
+Description: The purpose of this program is to take user inputs,
+store those values into variables then give an output
+based on calculations made with those inputs
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	
+	//User Inputs
+	string companyName;
+	int numHoursWorked;
+	int payRate;
+	
+
+	
+
+	cout << "What company do you work for? " << endl;
+	getline(cin, companyName);
+
+}
