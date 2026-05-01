ccc270c5f16d93459d5f02ed86e408faac01b81d
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-04-30 18:30:52 -0700

    submission for 9

---
+++ programchallengech3/challenge9_howmanycalories.cpp
@@ -0,0 +1,34 @@
+/*
+Team Name: PlusPlusUltra
+Team member: Xonie Martin, Kyungjae Lee
+Name: Kyungaejae Lee
+Assignment number: Programming Challenges – Chapter 3
+Description: This program will write a program
+that asks the user to input how many cookies he or she actually ate and then reports
+how many total calories were consumed.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	// Variables
+	double oneServing = 10; // One serving is 10 cookies
+	double servingCalories = 300; // One serving is equal to 300 calories.
+	double cookieAmount; // Amount cookies the user ate
+	double consumedCalories;
+
+	// Inputs
+	cout << "How many cookies did you eat?: ";
+	cin >> cookieAmount;
+
+	// Calculation
+	consumedCalories = (servingCalories / oneServing) * cookieAmount;
+
+	// Display
+	cout << "\nYou consumed " << consumedCalories << " calories." << endl;
+	return 0;
+
+}
