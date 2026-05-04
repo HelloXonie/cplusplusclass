ea2a58fe7539de029ad1236f55b96e01bd8e1cfe
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-05-04 11:30:48 -0700

    starting the assignment code portion

--- ch4/ch4_ex1.cpp
+++ ch4/ch4_ex1.cpp
@@ -5,8 +5,24 @@
 *
 */
 
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
 
 int main(){
+	string companyName;
+	int hoursWorked;
+	int hourlyRate;
+
+	cout << "Please tell me the name of your company: " << endl;
+	getling(cin, companyName);
+
+	cout << "How many hours did you work?" << endl;
+	cin >> hoursWorked;
+
+	cout << "How much is your hourly rate?" << endl
+	cin >> hourlyRate;
 
 
 
