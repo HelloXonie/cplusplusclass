138cfb9d1f6bd9b67a9776bf456c4fd88fd739fa
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-05-04 23:45:07 -0700

    starting assignment 2

---
+++ ch4/ch4_ex2.cpp
@@ -0,0 +1,20 @@
+/*
+Name: Xonie
+Team Name: Plus Plus Ultra - Xonie Martin, Kyungjae Lee
+Description: 
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	// Constants
+	const int REGULAR_HOURS = 40;
+
+	return 0;
+}
