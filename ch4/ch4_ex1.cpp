6e3b61141b5489ab4e24b5b98186d040a505cef6
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-05-04 23:20:19 -0700

    final submission

--- ch4/ch4_ex1.cpp
+++ ch4/ch4_ex1.cpp
@@ -10,16 +10,21 @@ hours.
 #include <iomanip>
 #include <cstdlib>
 #include <string>
+#include <ctime>
+
 using namespace std;
 
 int main() {
 	
 	// Constants
+	const int MIN_DOC = 1000;
+	const int MAX_DOC = 2000;
 	const double OVERTIME_PAY = 1.5;
-
+	
 	// Variables
 	string companyName;
 	int hoursWorked;
+	int docNumber;
 	double hourlyRate;
 	double regularGrossPay;
 	double overtimeGrossPay;
@@ -42,20 +47,53 @@ int main() {
 	}
 	else {
 		regularGrossPay = 40 * hourlyRate;
-		overtimeGrossPay = hoursWorked * (hourlyRate * OVERTIME_PAY);
+		overtimeGrossPay = (hoursWorked - 40) * (hourlyRate * OVERTIME_PAY);
 	}
 	
 	// Calculate total gross pay
 	totalGrossPay = regularGrossPay + overtimeGrossPay;
+
+	// Random number generator for document
+	srand(static_cast<unsigned int>(time(0))); // seed random number
+	docNumber = MIN_DOC + rand() % (MAX_DOC - MIN_DOC + 1);
 
 	// Display output
 	cout << fixed << setprecision(2) << showpoint; // format pay output
 	cout << "Company name: " << companyName << endl;
 	cout << "Hours worked: " << hoursWorked << endl;
 	cout << "Hourly rate: " << hourlyRate << endl;
-	cout << "Document number: " <<
+	cout << "Document number: " << docNumber << endl;
 	cout << "Your " << companyName << " gross pay is $" << totalGrossPay << " dollars." << endl;
 
 	return 0;
 }
 
+/********* Sample Output *********
+
+Please tell me the name of your company: 
+google
+How many hours did you work?
+50
+How much is your hourly rate?
+20
+Company name: google
+Hours worked: 50
+Hourly rate: 20.00
+Document number: 1839
+Your google gross pay is $1100.00 dollars.
+
+-----------------------------------
+
+Please tell me the name of your company: 
+Netflix
+How many hours did you work?
+40
+How much is your hourly rate?
+84.25
+Company name: Netflix
+Hours worked: 40
+Hourly rate: 84.25
+Document number: 1270
+Your Netflix gross pay is $3370.00 dollars.
+
+*/
