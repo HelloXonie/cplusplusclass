6e46323a74b083fe75370947bce35dbeec4ea9f5
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-05-04 22:54:21 -0700

    adding to assignment

--- ch4/ch4_ex1.cpp
+++ ch4/ch4_ex1.cpp
@@ -1,34 +1,60 @@
 /*
-*
-*
-*
-*
+Name: Xonie
+Team Name: Plus Plus Ultra - Xonie Martin, Kyungjae Lee
+Description: The purpose of this program is to take user inputs,
+and calculate gross pay based on if they did overtime or regular
+hours.
 */
 
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>
 #include <string>
 using namespace std;
 
-int main(){
+int main() {
+	
+	// Constants
+	const double OVERTIME_PAY = 1.5;
+
+	// Variables
 	string companyName;
 	int hoursWorked;
-	int hourlyRate;
+	double hourlyRate;
+	double regularGrossPay;
+	double overtimeGrossPay;
+	double totalGrossPay;
 
+	// Prompt the user for input
 	cout << "Please tell me the name of your company: " << endl;
-	getling(cin, companyName);
+	getline(cin, companyName);
 
 	cout << "How many hours did you work?" << endl;
 	cin >> hoursWorked;
 
-	cout << "How much is your hourly rate?" << endl
+	cout << "How much is your hourly rate?" << endl;
 	cin >> hourlyRate;
 
+	// check for how many hours worked to calculate total gross pay
+	if (hoursWorked <= 40) {
+		regularGrossPay = hoursWorked * hourlyRate;
+		overtimeGrossPay = 0;
+	}
+	else {
+		regularGrossPay = 40 * hourlyRate;
+		overtimeGrossPay = hoursWorked * (hourlyRate * OVERTIME_PAY);
+	}
+	
+	// Calculate total gross pay
+	totalGrossPay = regularGrossPay + overtimeGrossPay;
 
-
-
-
-
+	// Display output
+	cout << fixed << setprecision(2) << showpoint; // format pay output
+	cout << "Company name: " << companyName << endl;
+	cout << "Hours worked: " << hoursWorked << endl;
+	cout << "Hourly rate: " << hourlyRate << endl;
+	cout << "Document number: " <<
+	cout << "Your " << companyName << " gross pay is $" << totalGrossPay << " dollars." << endl;
 
 	return 0;
 }
