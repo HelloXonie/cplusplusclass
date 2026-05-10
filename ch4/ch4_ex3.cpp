248b478774ec5f9db957f68711625347f74cd3fd
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-05-10 10:36:04 -0700

    almost completed

--- ch4/ch4_ex3.cpp
+++ ch4/ch4_ex3.cpp
@@ -19,5 +19,46 @@ int main() {
 	// Constants
 	const int MIN_DOC = 1000;
 	const int MAX_DOC = 2000;
+	const int REGULAR_HOURS = 40;
 	const double OVERTIME_PAY = 1.5;
+
+	// Variables
+	string companyName;
+	int hoursWorked;
+	int docNumber;
+	double hourlyRate;
+	double grossPay;
+	double totalGrossPay;
+
+	// Prompt the user for input
+	cout << "Please tell me the name of your company: " << endl;
+	getline(cin, companyName);
+
+	cout << "How many hours did you work?" << endl;
+	cin >> hoursWorked;
+
+	cout << "How much is your hourly rate?" << endl;
+	cin >> hourlyRate;
+
+	// check for how many hours worked to calculate total gross pay
+	if (hoursWorked > REGULAR_HOURS) {
+		grossPay = (REGULAR_HOURS * hourlyRate) + ((hoursWorked - REGULAR_HOURS) * (hourlyRate * OVERTIME_PAY));
+	}
+	else {
+		grossPay = hoursWorked * hourlyRate;
+	}
 	
+	// Random number generator for document
+	srand(static_cast<unsigned int>(time(0))); // seed random number
+	docNumber = MIN_DOC + rand() % (MAX_DOC - MIN_DOC + 1);
+
+	// Display output
+	cout << fixed << setprecision(2) << showpoint; // format pay output
+	cout << "Company name: " << companyName << endl;
+	cout << "Hours worked: " << hoursWorked << endl;
+	cout << "Hourly rate: " << hourlyRate << endl;
+	cout << "Document number: " << docNumber << endl;
+	cout << "Your " << companyName << " gross pay is $" << grossPay << " dollars." << endl;
+
+	return 0;
+}
