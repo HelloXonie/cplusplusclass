f36440c059139ff4b00536c94c03f39c6f4b52ff
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-05-10 10:41:33 -0700

    cleaned up some code and added sample output

--- ch4/ch4_ex3.cpp
+++ ch4/ch4_ex3.cpp
@@ -28,7 +28,6 @@ int main() {
 	int docNumber;
 	double hourlyRate;
 	double grossPay;
-	double totalGrossPay;
 
 	// Prompt the user for input
 	cout << "Please tell me the name of your company: " << endl;
@@ -62,3 +61,33 @@ int main() {
 
 	return 0;
 }
+
+/************** Sample Output **************
+
+Please tell me the name of your company: 
+Facebook
+How many hours did you work?
+68
+How much is your hourly rate?
+82.50
+Company name: Facebook
+Hours worked: 68
+Hourly rate: 82.50
+Document number: 1563
+Your Facebook gross pay is $6765.00 dollars.
+
+**********************************************
+
+Please tell me the name of your company: 
+NASA
+How many hours did you work?
+61 
+How much is your hourly rate?
+97.63
+Company name: NASA
+Hours worked: 61
+Hourly rate: 97.63
+Document number: 1834
+Your NASA gross pay is $6980.55 dollars.
+
+*/
