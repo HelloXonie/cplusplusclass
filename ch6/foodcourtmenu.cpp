4393f512a88d114fb470d903a0f3853a6bacfc87
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2026-05-23 17:27:31 -0700

    first commit menu project

---
+++ ch6/foodcourtmenu.cpp
@@ -0,0 +1,26 @@
+/*
+Name: Xonie Martin
+Team Name: Plus Plus Ultra - Xonie Martin, Kyungjae Lee
+Date: May 23, 2026
+Description: The purpose of this program is to build a menu, and take user inputs for an order.
+The program will handle calculation and output a final bill to a separate file.
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+
+}
